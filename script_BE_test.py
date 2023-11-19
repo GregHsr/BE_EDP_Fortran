453c7af225f9fdb13efd9fa5effb5ec3920aee01
GregHsr <gregoire.husser@etu.inp-n7.fr>
2023-11-19 14:52:59 +0100

    nouveau script de test

--- script_BE_test.py
+++ script_BE_test.py
@@ -6,14 +6,18 @@ import numpy as np
 
 # Valeurs à tester 
 R = np.geomspace(0.5, 500, 20)
-W = np.linspace(0.1, 1.9, 100)
-R0 = np.geomspace(0.000001, 10, 50)
+W = np.linspace(0.1, 1.9, 20)
+R0 = np.linspace(0.0001, 10, 20)
 L_R_exp = []
 L_temps_execution_imp_Gauss = []
 L_temps_execution_imp_Sor = []
 L_temps_execution_exp = []
 L_temps_execution_W = []
 L_temps_execution_R0 = []
+
+# Faire varier R0 et W en même temps
+
+L_temps_execution_R0_W = [[0 for i in range(len(W))] for j in range(len(R0))]
 
 # lecture des données et sauvegarde
 fichier = open("./CODE_IMPLICITE_GAUSS/data.txt", "r")
@@ -170,38 +174,39 @@ fichier_resultats = open("resultats.csv", "w")
 
 
 for k in range(len(R0)): # /!\ ou W
-    # Changer la valeur de W   
-    fichier = open("./CODE_IMPLICITE_GAUSS/data.txt", "w")
-    for i in range(len(data)):
-        if i == 16:   # Test R0
-        # if i == 17: # Test W
-            fichier.write(str(R0[k])+" !"+"\n")
+    for l in range(len(W)):
+        # Changer la valeur de W   
+        fichier = open("./CODE_IMPLICITE_GAUSS/data.txt", "w")
+        for i in range(len(data)):
+            if i == 16:   # Test R0
+                fichier.write(str(R0[k])+" !"+"\n")
+            elif i == 17:   
+                fichier.write(str(W[l])+" !"+"\n")
+            else:
+                fichier.write(data[i])
+        fichier.close()
+
+        # Commande Bash qui exécute le script.sh
+        commande_bash = "./script.sh"
+
+        # Exécute la commande Bash et capture la sortie
+        result = subprocess.run(commande_bash, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        # Vérifie si la commande s'est exécutée avec succès (code de retour 0)
+        if result.returncode == 0:
+            # Recherche du motif du temps d'exécution dans la sortie d'erreur
+            temps_execution = re.search(r'real\s+(\d+m[\d.]+s)', result.stderr)
+            if temps_execution:
+                # Récupère le temps d'exécution complet (y compris les minutes et les secondes)
+                temps_execution_str = temps_execution.group(1)
+                print("Temps d'exécution complet :", temps_execution_str)
+                L_temps_execution_R0_W[k][l]=temps_execution_str 
+            else:
+                print("Le temps d'exécution n'a pas été trouvé dans la sortie d'erreur.")
         else:
-            fichier.write(data[i])
-    fichier.close()
-
-    # Commande Bash qui exécute le script.sh
-    commande_bash = "./script.sh"
-
-    # Exécute la commande Bash et capture la sortie
-    result = subprocess.run(commande_bash, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    # Vérifie si la commande s'est exécutée avec succès (code de retour 0)
-    if result.returncode == 0:
-        # Recherche du motif du temps d'exécution dans la sortie d'erreur
-        temps_execution = re.search(r'real\s+(\d+m[\d.]+s)', result.stderr)
-        if temps_execution:
-            # Récupère le temps d'exécution complet (y compris les minutes et les secondes)
-            temps_execution_str = temps_execution.group(1)
-            print("Temps d'exécution complet :", temps_execution_str)
-            L_temps_execution_R0.append(temps_execution_str) # Test R0
-            #L_temps_execution_W.append(temps_execution_str) # Test W
-        else:
-            print("Le temps d'exécution n'a pas été trouvé dans la sortie d'erreur.")
-    else:
-        # Affiche les erreurs si la commande a échoué
-        print("La commande Bash a échoué. Erreurs :")
-        print(result.stderr)
+            # Affiche les erreurs si la commande a échoué
+            print("La commande Bash a échoué. Erreurs :")
+            print(result.stderr)
 
 
 # Nettoyage des fichiers temporaires
@@ -221,11 +226,12 @@ fichier_resultats.close()
     #temps = float(temps[0])*60+float(temps[1][:-1])
     #L_temps_execution_W[k] = temps
 
+print(L_temps_execution_R0_W)
 for k in range(len(R0)):
-    # Test R0
-    temps = L_temps_execution_R0[k].split("m")
-    temps = float(temps[0])*60+float(temps[1][:-1])
-    L_temps_execution_R0[k] = temps
+    for l in range(len(W)):
+        temps = L_temps_execution_R0_W[k][l].split("m")
+        temps = float(temps[0])*60+float(temps[1][:-1])
+        L_temps_execution_R0_W[k][l] = temps
 
 
 #plt.figure(2)
@@ -234,9 +240,17 @@ for k in range(len(R0)):
 #plt.ylabel("Temps d'exécution (s)")
 #plt.show()
 
-plt.figure(3)
-plt.plot(R0, L_temps_execution_R0, 'o--')
-plt.xlabel("Valeur de R0")
-plt.ylabel("Temps d'exécution (s)")
-plt.show()
+#plt.figure(3)
+#plt.plot(R0, L_temps_execution_R0, 'o--')
+#plt.xlabel("Valeur de R0")
+#plt.ylabel("Temps d'exécution (s)")
+#plt.show()
 
+plt.figure(4)
+# map en 2D avec le temps en ordonnée avec titre pour la barre
+im = plt.imshow(L_temps_execution_R0_W,extent=[0.1, 1.9, 0.0001, 10],origin='lower', cmap='hot', interpolation='bilinear', aspect='auto')
+cbar = plt.colorbar(im)
+cbar.set_label('Temps d\'exécution (s)')
+plt.xlabel("Valeur de W")
+plt.ylabel("Valeur de R0")
+plt.show()
